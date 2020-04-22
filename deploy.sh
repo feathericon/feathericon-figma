b7f0479031a25ed559a137b9e3a7c5c1144ac33e
featherplain <megumi@featherplain.com>
2020-04-22 20:08:00 +0900

    set travis ci env

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#! bash
+
+set -e
+
+rm -rf .git
+rm -r .gitignore
+rm -rf download-feathericon.png
+rm -rf feathericon-figma.png
+rm -rf package.json
+rm -rf README.md
+rm -ef export.js
+
+if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+	echo "Not deploying pull requests."
+	exit
+fi
+
+if [[ "master" != "$TRAVIS_BRANCH" ]]; then
+	echo "Not on the 'master' branch."
+	exit
+fi
+
+echo "*.json
+*.yml
+.DS_store
+*.log
+.npmignore" > .gitignore
+
+git init
+git config user.name "featherplain"
+git config user.email "${GH_USER_EMAIL}"
+git add .
+git commit --quiet -m "Deploy from travis"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:release > /dev/null 2>&1
