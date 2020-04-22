814de17fee500926134a93eb9fddcbd514d92ea9
featherplain <megumi@featherplain.com>
2020-04-22 20:12:49 +0900

    modify export.js

--- export.js
+++ export.js
@@ -1,4 +1,4 @@
-const FIGMA_TOKEN = '${FIGMA_TOKEN}'
+const FIGMA_TOKEN = '42497-d4256642-d663-4308-8121-5a587c407219}'
 const FIGMA_FILE_URL = 'https://www.figma.com/file/K7LUOW5a89CttKlexrd3PAP6/feathericon?version-id=301203044&node-id=0%3A1&viewport=86%2C25%2C3.1148648262023926'
 
 const got = require('got')
